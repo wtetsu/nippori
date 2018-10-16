d2311002e50207c6258ad7e71ab95e70e68dc44b
wtetsu <tewatana@gmail.com>
2018-10-16 19:54:48 +0900

    Create record structures

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="width:500px;height:550px;overflow:hidden;">
+  <div style="width:600px;height:550px;overflow:hidden;">
     <input type="button" value="前の動画へ" @click="gotoPrev()" />
     <input type="button" value="次の動画へ" @click="gotoNext()" />
     <input type="button" value="更新" @click="fetchRecords()" />
@@ -7,6 +7,7 @@
     <br />
 
     <input type="text" v-model="searchText" v-on:keyup="searchTextChanged()" @change="searchTextChanged()" ref="searchText" />
+    {{searchResult}}
 
     <paginate v-model="currentPage" :page-count="pageCount" :click-handler="onClickPagination" :prev-text="'Prev'"
       :next-text="'Next'" :active-class="'current'" :container-class="'pagination'" :page-range="7">
@@ -17,12 +18,15 @@
     </div>
 
     <div style="height:350px;overflow-y:scroll;">
-      <div v-for="r in filteredRecords" :key="r.contentId">
-        <a @click=" jumpToTheMovie(r.contentId)">
-          <img class="img" v-bind:src="r.thumbnailUrl" v-on:mouseover="mouseover(r.description)" style="cursor:pointer;" width="65" height="50" />
-        </a>
-        <span v-bind:title="r.description">
-          {{ r.title }}
+      <div v-for="dateRecords in filteredRecords" :key="dateRecords.date">
+        <h3>{{dateRecords.date}}</h3>
+        <span v-for="r in dateRecords.records" :key="r.contentId">
+          <a @click=" jumpToTheMovie(r.contentId)">
+            <img class="img" v-bind:src="r.thumbnailUrl" v-on:mouseover="mouseover(r.description)" style="cursor:pointer;" width="65" height="50" />
+          </a>
+          <span v-bind:title="r.description">
+            <!-- {{ r.title }} -->
+          </span>
         </span>
       </div>
     </div>
@@ -48,7 +52,8 @@ export default {
       currentPage: 1,
       pageSize: 200,
       pageCount: 1,
-      description: "ここに動画の説明が表示されます。"
+      description: "ここに動画の説明が表示されます。",
+      searchResult: "100 ms"
     };
   },
   async created() {
@@ -148,6 +153,52 @@ export default {
       const searchText = this.searchText ? this.searchText.trim() : "";
       const startPosition = this.pageSize * (this.currentPage - 1);
 
+      // let filteredRecords;
+      // if (searchText) {
+      //   filteredRecords = this.records.filter(a => {
+      //     return a.description.includes(searchText);
+      //   });
+      // } else {
+      //   filteredRecords = this.records;
+      // }
+
+      const result = [
+        // {
+        //   date: "20070101",
+        //   records: []
+        // },
+        // {
+        //   date: "20070108",
+        //   records: []
+        // },
+      ];
+      let dateRecords = null;
+      for (let i = 0; i < this.records.length; i++) {
+        const rec = this.records[i];
+        if (dateRecords == null || dateRecords.date !== rec.date) {
+          if (dateRecords) {
+            result.push(dateRecords);
+          }
+          dateRecords = {
+            date: rec.date,
+            records: []
+          };
+        }
+        dateRecords.records.push(rec);
+      }
+      if (dateRecords) {
+        result.push(dateRecords);
+      }
+      console.info(result);
+      return result;
+    },
+
+    filteredRecords_() {
+      const startTime = new Date().getTime();
+
+      const searchText = this.searchText ? this.searchText.trim() : "";
+      const startPosition = this.pageSize * (this.currentPage - 1);
+
       let filteredRecords;
       if (searchText) {
         filteredRecords = this.records.filter(a => {
@@ -162,11 +213,15 @@ export default {
       //   this.currentPage = this.pageCount;
       // }
 
-      const computedRecords = filteredRecords.slice(startPosition, startPosition + this.pageSize);
-      return computedRecords.map(a => {
+      const computedRecords = filteredRecords.slice(startPosition, startPosition + this.pageSize).map(a => {
         a.title = normalizeTitleText(a.title);
         return a;
       });
+
+      const time = new Date().getTime() - startTime;
+      this.searchResult = `${filteredRecords.length}件(${time}ms)`;
+
+      return computedRecords;
     }
   }
 };
