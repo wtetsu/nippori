0172fd2d7a89b79fed9d909aad604caabf36fbd9
wtetsu <tewatana@gmail.com>
2018-10-23 19:22:55 +0900

    Fix tests

--- __test__/text.test.js
+++ __test__/text.test.js
@@ -1,22 +1,20 @@
+import text from "../src/text.js";
+
 test("", async () => {
-  replace("あいうえお", ["あ"], t => "<span>" + t + "</span>");
+  expect(text.replace("あいうえお", ["ああ"], t => "<span>" + t + "</span>")).toEqual("あいうえお");
+  expect(text.replace("あいうえお", ["あ"], t => "<span>" + t + "</span>")).toEqual("<span>あ</span>いうえお");
+  expect(text.replace("あいうえお", ["ああ"], t => "<span>" + t + "</span>")).toEqual("あいうえお");
 
-  expect(replace("あいうえお", ["ああ"], t => "<span>" + t + "</span>")).toEqual("あいうえお");
-  expect(replace("あいうえお", ["あ"], t => "<span>" + t + "</span>")).toEqual("<span>あ</span>いうえお");
-  expect(replace("あいうえお", ["ああ"], t => "<span>" + t + "</span>")).toEqual("あいうえお");
-
-  expect(replace("いいいああいいいああいいい", ["ああ"], t => "<span>" + t + "</span>")).toEqual(
+  expect(text.replace("いいいああいいいああいいい", ["ああ"], t => "<span>" + t + "</span>")).toEqual(
     "いいい<span>ああ</span>いいい<span>ああ</span>いいい"
   );
 
-  expect(replace("あいうえお", ["あい", "いう", "うえ"], t => "<span>" + t + "</span>")).toEqual(
-    "<span>あい</span><span>うえ</span>お"
-  );
+  expect(text.replace("あいうえお", ["あい", "いう", "うえ"], t => "<span>" + t + "</span>")).toEqual("<span>あい</span><span>うえ</span>お");
 
-  expect(replace("abc", ["a", "a", "a"], t => "<span>" + t + "</span>")).toEqual("<span>a</span>bc");
-  expect(replace("abcabc", ["a"], t => "<span>" + t + "</span>")).toEqual("<span>a</span>bc<span>a</span>bc");
+  expect(text.replace("abc", ["a", "a", "a"], t => "<span>" + t + "</span>")).toEqual("<span>a</span>bc");
+  expect(text.replace("abcabc", ["a"], t => "<span>" + t + "</span>")).toEqual("<span>a</span>bc<span>a</span>bc");
 
-  expect(replace("abcabcabcabcabcabc", ["a", "a", "a"], t => "<span>" + t + "</span>")).toEqual(
+  expect(text.replace("abcabcabcabcabcabc", ["a", "a", "a"], t => "<span>" + t + "</span>")).toEqual(
     "<span>a</span>bc<span>a</span>bc<span>a</span>bc<span>a</span>bc<span>a</span>bc<span>a</span>bc"
   );
 });
