979a44c65c4613ee6ee5f39befc5b62a04a29119
wtetsu <tewatana@gmail.com>
2019-02-09 09:33:06 +0900

    Update packages

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,6 +1,8 @@
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 const { VueLoaderPlugin } = require("vue-loader");
+
+const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   mode: process.env.NODE_ENV || "development",
@@ -40,21 +42,18 @@ module.exports = {
       vue$: "vue/dist/vue.esm.js"
     }
   },
-  optimization:
-    process.env.NODE_ENV === "production"
-      ? {
-          minimizer: [
-            new UglifyJsPlugin({
-              uglifyOptions: {
-                compress: true,
-                ecma: 6,
-                mangle: true
-              },
-              sourceMap: false
-            })
-          ]
+  devtool: isProd ? false : "cheap-module-source-map",
+  optimization: {
+    minimize: isProd,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: true,
+          warnings: false
         }
-      : {},
+      })
+    ]
+  },
   plugins: [new CopyWebpackPlugin([{ from: "static", to: "." }]), new VueLoaderPlugin()],
   devtool: process.env.NODE_ENV === "production" ? false : "cheap-module-source-map"
 };
